df81a82a51608f2e7814eb197734efc79b836550
diegopasti <diegopasti@gmail.com>
2023-08-15 03:02:36 -0300

    Criei um arquivo css e js para customização do admin.

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from django.core.exceptions import FieldError
+from django.utils.html import format_html
+
+from django_admin_filters import MultiChoice
+
+
+class StatusFilter(MultiChoice):
+    FILTER_LABEL = "By status"
 
 
 class BaseModelAdmin(admin.ModelAdmin):
+
+    class Media:
+        css = {
+            'all': ('core/css/admin.css',)
+        }
+        js = ['core/js/admin.js']
+
     def save_model(self, request, obj, form, change):
         """ Save user than create or update object on admim """
 
@@ -14,3 +28,20 @@ class BaseModelAdmin(admin.ModelAdmin):
             pass
 
         super().save_model(request, obj, form, change)
+
+    def column_separator(self, obj):
+        message = format_html("<div style='width:0px;'></div>")
+        return message
+
+    def double_row(self, first_value, second_value):
+        message = format_html(
+            f"<div style='text-align:center;white-space: nowrap;'>"
+            f"{first_value}<br><span style='font-size:9px;color:#777;'>"
+            f"{second_value}</span>"
+            f"</div>"
+        )
+        return message
+
+    column_separator.allow_tags = False
+    column_separator.short_description = " "
+
--- apps/game/admin.py
+++ apps/game/admin.py
@@ -1,15 +1,29 @@
 from django.contrib import admin
 from django.db import models
 from django.forms import TextInput, Select, SelectMultiple
-from django.utils.html import format_html
 
-from apps.core.admin import BaseModelAdmin
+from apps.core.admin import BaseModelAdmin, StatusFilter
 from apps.game.forms import CreationPlayerForm
 from apps.game.models import Player
 
 
 @admin.register(Player)
 class PlayerAdmin(BaseModelAdmin):
+
+    actions_on_bottom = True
+    actions_on_top = False
+    search_fields = ['name']
+    list_filter = [
+        "gols",
+        "best_of_match",
+        "best_of_team",
+        #("created_by", StatusFilter),
+        "created_by",
+        "updated_by",
+    ]
+
+    add_form = CreationPlayerForm
+
     formfield_overrides = {
         models.CharField: {'widget': TextInput(attrs={"style": "min-width:100vh"})},
         models.IntegerField: {'widget': TextInput(attrs={"style": "min-width:100vh"})},
@@ -19,28 +33,14 @@ class PlayerAdmin(BaseModelAdmin):
         models.DurationField: {'widget': TextInput(attrs={"style": "width:100%"})},
         models.ForeignKey: {'widget': Select(attrs={"style": "min-width:100vh"})},
         models.ManyToManyField: {'widget': SelectMultiple(attrs={"style": "min-width:100vh"})},
-
-        #models.BooleanField: {'widget': CheckboxInput(attrs={"style": "float:right;'"})},
-        #models.ForeignKey: {'widget': Select(attrs={'size': '100'})},
-        #models.TextField: {'widget': Textarea(attrs={'rows': 4, 'cols': 40})},
     }
 
-    add_form = CreationPlayerForm
-
     list_display = (
-        "name",
-        "best_per_team", "best_per_match",  # "matchs", "rounds",
-        "gols_per_match", "victories_result", "losses_result", "empaths_result",
+        "name", "column_separator",
+        "best_per_team", "best_per_match", "column_separator",
+        "gols_per_match", "victories_result", "losses_result", "empaths_result", "column_separator",
         "created", "updated"
     )
-
-    # fields = (
-    #     "name",
-    #     "victories", "victories_rate", "empaths", "empaths_rate", "losses", "losses_rate",
-    #
-    #     "best_of_team", "best_of_match", "total_matchs", "total_rounds", "gols", "gols_rate", "matchs", "rounds",
-    #     "created_by", "creation_date", "updated_by", "last_update", "is_active",
-    # )
 
     fieldsets = (
         ("Jogador", {
@@ -73,7 +73,7 @@ class PlayerAdmin(BaseModelAdmin):
         return super().get_form(request, obj, **defaults)
 
     def gols_per_match(self, obj):
-        return self.double_row(f"{obj.gols}", f"{obj.gols_rate} por partida")
+        return self.double_row(f"{obj.gols}", f"{obj.gols_rate} POR PARTIDA")
 
     def victories_result(self, obj):
         return self.double_row(f"{obj.victories}", f"{obj.victories_rate}%")
@@ -85,51 +85,52 @@ class PlayerAdmin(BaseModelAdmin):
         return self.double_row(f"{obj.empaths}", f"{obj.empaths_rate}%")
 
     def best_per_match(self, obj):
-        return self.double_row(f"{obj.best_of_match} vezes", f"em {obj.total_matchs} Partidas")
+        return self.double_row(f"{obj.best_of_match} vezes", f"EM {obj.total_matchs} PARTIDAS")
 
     def best_per_team(self, obj):
-        return self.double_row(f"{obj.best_of_team} vezes", f"em {obj.total_matchs} Partidas")
+        return self.double_row(f"{obj.best_of_team} vezes", f"EM {obj.total_matchs} PARTIDAS")
 
     def created(self, obj):
         return self.double_row(
-            f"{obj.creation_date.strftime('%d/%m/%Y às %H:%M:%S')}", f"por {obj.created_by}"
+            f"{obj.creation_date.strftime('%d/%m/%Y às %H:%M:%S')}", f"POR {obj.created_by.username.upper()}"
         )
 
     def updated(self, obj):
         return self.double_row(
-            f"{obj.last_update.strftime('%d/%m/%Y às %H:%M:%S')}", f"por {obj.updated_by}"
+            f"{obj.last_update.strftime('%d/%m/%Y às %H:%M:%S')}", f"POR {obj.updated_by.username.upper()}"
         )
-
-    def double_row(self, first_value, second_value):
-        message = format_html(
-            f"<div style='text-align:center;white-space: nowrap;'>{first_value}<br><sub style='color:#777;'>{second_value}</sub></div>")
-        return message
 
     gols_per_match.allow_tags = True
     gols_per_match.short_description = "Gols"
+    gols_per_match.admin_order_field = 'gols'
 
     victories_result.allow_tags = True
     victories_result.short_description = "Vitórias"
+    victories_result.admin_order_field = 'victories'
 
     empaths_result.allow_tags = True
     empaths_result.short_description = "Empates"
+    empaths_result.admin_order_field = 'empaths'
 
     losses_result.allow_tags = True
     losses_result.short_description = "Derrotas"
+    losses_result.admin_order_field = 'losses'
 
     best_per_match.allow_tags = True
     best_per_match.short_description = "Melhor da Partida"
+    best_per_match.admin_order_field = 'best_of_match'
 
     best_per_team.allow_tags = True
     best_per_team.short_description = "Melhor do Time"
+    best_per_team.admin_order_field = 'best_of_team'
 
     created.allow_tags = True
     created.short_description = "Cadastrado em"
+    created.admin_order_field = 'creation_date'
 
     updated.allow_tags = True
     updated.short_description = "Atualizado em"
-
-
+    updated.admin_order_field = 'last_updated'
 
     #form = SpecialityAdminForm
     #list_display = ("name", "question_count", "main_speciality", "all_tags",)
